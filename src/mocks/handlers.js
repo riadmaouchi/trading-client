ba37bf48d89b0c6eac3c84829a45bc3e5847dd9d
Riad <riad.maouchi@gmail.com>
2022-03-07 22:07:18 +0100

    fix mock user response delay

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -4,8 +4,9 @@ export const handlers = [
     rest.get('http://localhost:8080/api/v1/users/:userId', (req, res, ctx) => {
         const { userId } = req.params
         console.log('userId', userId)
-        ctx.delay(2000)
+
         return res(
+            ctx.delay(2000),
             ctx.status(200),
             ctx.json({
                 firstName: 'Arvel',
