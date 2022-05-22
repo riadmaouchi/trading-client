7f27a3cad2bcf2ee2747a21210b4895134146c17
Riad <riad.maouchi@gmail.com>
2022-05-22 19:41:56 +0200

    setup storybook

---
+++ src/App.stories.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+import App from "./App"
+
+export default {
+  component: App,
+  title: "App",
+}
+
+// eslint-disable-next-line react/function-component-definition
+const Template = () => <App />
+
+export const Default = Template.bind({})
